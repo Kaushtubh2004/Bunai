159c6384ec58f7e51f60dfd478d28eb6038711d5
Kaushtubh <kaushtubh@gmail.com>
2025-11-21 13:26:15 +0530

    fixed index

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000,"0.0.0.0", ()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
